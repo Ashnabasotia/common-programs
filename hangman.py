32ae5ed5ce54b62d2b65ec4c340f496594e865ab
Namit Sinha <realnamitsinha@gmail.com>
2015-08-02 13:49:28 +0530

    update

--- hangman.py
+++ hangman.py
@@ -17,12 +17,11 @@ def Cutoff(word,cuts):
 	word_=""
 	lis=[]
 	length=len(word)
-	for i in range(0,length):
-		lis.append(i)
+	for i in range(0,length):lis.append(i)
 	random.shuffle(lis)
-	print(lis)
+	#print(lis)
 	lis=lis[0:int(length*cuts)]
-	print(lis)
+	#print(lis)
 	for i in range(0,length):
 		if(i not in lis):
 			word_+="-"
@@ -33,17 +32,42 @@ def Cutoff(word,cuts):
 def start_game(iter):
 	for i in range(iter):hangman()
 
+def checkHangman(word,shown):
+	chances=5
+	while(chances>0):
+		letter=input()
+		found=0
+		for i in range(len(word)):
+			if(letter==word[i] and shown[i]=='-'):
+				found=1
+				break
+		if(found==1):
+			words=""
+			for i in range(len(word)):
+				if(word[i]==letter and shown[i]=='-'):
+					words+=word[i]
+				else:
+					words+=shown[i]
+			shown=words
+			if(shown==word):
+				return 1
+			print(shown)
+		else:
+			chances-=1
+			print("Wrong Guess "+str(chances)+" chances left")
+			print(shown)
+	return 0
+
 def hangman():
 	print("guess this word")
 	word=Select(5)
-	display=Cutoff(word,0.60)
+	display=Cutoff(word,0.70)
 	print(display)
-	inps=input()
-	if(inps==word):
+	result=checkHangman(word,display)
+	if(result==1):
+		print(word)
 		print("You Won")
 	else:
 		print("You Lost ,The word was "+word)
 
 start_game(1)
-
-
